acc587811c515e4bb46eeadef440e1b7cfad131a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-05 01:47:05 +0000

    introduce variables

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,8 +1,11 @@
 from devprocess import echo_value
 # Example usage of echo_value function
 def main():
-    print(echo_value("Hello, world!"))
-    print(echo_value(5))
+    echoed_value = echo_value("Hello, world!")
+    print(echoed_value)  # Output: Hello, world!
+
+    echoed_value = echo_value(42)
+    print(echoed_value)  # Output: 42
 
 if __name__ == "__main__":
     main()
