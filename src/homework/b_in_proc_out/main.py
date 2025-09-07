45198a281fef4cd1cc63b4fea9c8d13f97b733f5
Joshua Alvarez <joshua.alvarez2@g.austincc.edu>
2025-09-07 04:16:55 +0000

    hw01 correction

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,11 +1,8 @@
 from output import multiply_numbers
 
-SET_ONE = 7 x 7 
-SET_TWO = 5 x 5
-
 def main():
-    print(SET_ONE , multiply_numbers (7,7))
-    print(SET_TWO, multiply_numbers (5,5))
+    print('7 x 7 =' , multiply_numbers (7,7))
+    print('5 x 5 =' , multiply_numbers (5,5))
 
 if __name__ == "__main__":
     main()
