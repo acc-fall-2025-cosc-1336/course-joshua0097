cf2bdc87ff27ebfef4323a0d9fca33a18aa25598
Joshua Alvarez <joshua.alvarez2@g.austincc.edu>
2025-09-07 02:03:07 +0000

    Homework 1 update

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,8 +1,8 @@
 from output import multiply_numbers
 
 def main():
-    print(multiply_numbers(7, 7))
-    print(multiply_numbers(5, 5))
+    print('7 X 7 =', multiply_numbers (7,7))
+    print('5 X 5 =', multiply_numbers (5,5))
 
 if __name__ == "__main__":
     main()
