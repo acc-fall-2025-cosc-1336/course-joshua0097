1bb4f25164f9cbee2b6223b5e20402d9bad59921
Joshua Alvarez <joshua.alvarez2@g.austincc.edu>
2025-09-14 15:23:13 +0000

    hw02 function inputs

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,10 +1,17 @@
-from output import multiply_numbers
+from output import get_meal_amount, get_sales_tax_amount, get_tip_amount
+
+tax_rate = 6.75 / 100  # 6.75% as a decimal
+meal_amount = get_meal_amount()
+tip_amount = get_tip_amount()
 
 def main():
-    result_1 = multiply_numbers (7,7)
-    print('7 x 7 =', result_1)
-    result_2 = multiply_numbers (5,5)
-    print('5 x 5 =', result_2)
+    sales_tax_amount = get_sales_tax_amount(meal_amount, tax_rate)
+    total_bill = meal_amount + sales_tax_amount + tip_amount
 
+    print(f"Meal Amount: ${meal_amount:.2f}")
+    print(f"Sales Tax Amount: ${sales_tax_amount:.2f}")
+    print(f"Tip Amount: ${tip_amount:.2f}")
+    print(f"Total Bill: ${total_bill:.2f}")
+    
 if __name__ == "__main__":
     main()
--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,3 +1,8 @@
-def multiply_numbers(num1, num2):
-    return num1 * num2
+def get_sales_tax_amount(meal_amount, tax_rate):
+    return meal_amount * tax_rate
 
+def get_meal_amount():
+    return float(input("Enter the meal amount (as a decimal): "))
+
+def get_tip_amount():
+    return float(input("Enter the tip amount (as a decimal): "))
