7cf911a337a12d146c9c70c5dd12e9dbf6227803
Joshua Alvarez <joshua.alvarez2@g.austincc.edu>
2025-09-20 15:29:45 +0000

    hw03 work

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -4,11 +4,11 @@ from src.homework.c_decisions.decisions import get_letter_grade
 
 class Test_Config(unittest.TestCase):
 
-    def test_get_letter_grade(self):
+    def test_get_letter_grade(self): # tested
         self.assertEqual(get_letter_grade(95), 'A')
         self.assertEqual(get_letter_grade(85), 'B')
         self.assertEqual(get_letter_grade(75), 'C')
         self.assertEqual(get_letter_grade(65), 'D')
         self.assertEqual(get_letter_grade(50), 'F')
-        
+         
        
