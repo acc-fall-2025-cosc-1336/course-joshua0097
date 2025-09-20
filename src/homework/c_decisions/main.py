fd4250ac4c26bf833fe92caa752181b0f861bb5e
Joshua Alvarez <joshua.alvarez2@g.austincc.edu>
2025-09-20 19:56:30 +0000

    hw03 update (2)

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,7 +1,6 @@
 from decisions import get_letter_grade
 
 def main():
-
     grade = int(input("Enter your grade (0-100): "))
     letter_grade = get_letter_grade(grade)
     print(f"Your letter grade is: {letter_grade}")
