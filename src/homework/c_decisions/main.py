5ce1db3f1af39beed1cd38d3018ac2d3645dc2ea
Joshua Alvarez <joshua.alvarez2@g.austincc.edu>
2025-09-27 03:08:33 +0000

    hw 03 #2 completion

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,7 +1,7 @@
 from decisions import get_options_ratio, get_faculty_rating
 
 def main():
-    options = int(input("Enter the number of options: "))
+    options = float(input("Enter the number of options: "))
     total = float(input("Enter the total: "))
     ratio = get_options_ratio(options, total)
     rating = get_faculty_rating(ratio)
